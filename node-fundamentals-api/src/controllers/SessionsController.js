5c8aa56c73050cc70974976e75ba109337e657f0
Franklin Macedo Dias <frankmcdias@gmail.com>
2024-04-12 23:18:48 -0300

    feat: add token JWT

--- node-fundamentals-api/src/controllers/SessionsController.js
+++ node-fundamentals-api/src/controllers/SessionsController.js
@@ -1,5 +1,9 @@
 const knex = require('../database/knex')
 const AppError = require('../utils/AppError')
+const { compare } = require('bcryptjs')
+const authConfig = require('../configs/auth')
+const { sign } = require('jsonwebtoken')
+
 
 class SessionsController {
     async create(request, response) {
@@ -10,8 +14,20 @@ class SessionsController {
         if (!user) {
             throw new AppError('E-mail or password invalid!', 401)
         }
+
+        const passwordMatched = await compare(password, user.password)
+
+        if (!passwordMatched) {
+            throw new AppError('E-mail or password invalid!', 401)
+        }
+
+        const { secret, expiresIn } = authConfig.jwt
+        const token = sign({}, secret, {
+            subject: String(user.id),
+            expiresIn
+        })
         
-        return response.json(user)
+        return response.json({ user, token })
     }
 }
 
